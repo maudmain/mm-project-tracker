97d1cc5f203f018c5f662fa57156e820e282d011
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-27 15:18:38 +0000

    reference to DOM element added

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,11 +1,19 @@
 // reference to the DOM elements (create variables)
+const addProjectButton =$("#project-btn");
+let projectNameInput =$("#project-name-input");
+let projectTypeInput =$("#project-type");
+let hourlyRate =$("#hourly-rate-input");
+let datepicker =$("datepicker");
+const saveProjectButton =$("#save-btn")
 
 
-// display the current time with a setInterval to refresh every minute
+// display the current time with a setInterval to refresh
 $(document).ready(function () {
     setInterval(function () {
         const today = moment();
-        $("#currentTime").text(today.format('MMMM Do YYYY, h:mm:ss'));
+        $("#currentTime").text(today.format('MMMM Do YYYY, h:mm:ss a'));
     },
     1000);
 })
+
+// 
